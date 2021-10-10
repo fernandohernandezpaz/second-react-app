2cce10cfe2abfa10f6590ef6e0278189097d83ce
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2021-10-09 19:50:38 -0600

    utlizando useContext para obtener valores de context heredados del componente padre

--- src/Components/ComponentB.js
+++ src/Components/ComponentB.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import ComponentC from './ComponentC';
+import {NameContext, ColorContext} from '../App';
 
 const ComponentB = () => {
+    const userName = useContext(NameContext);
+    const color = useContext(ColorContext);
     return (
         <>
             <h3>Component B</h3>
-            <ComponentC />
+            <p>Name: {userName}, color <span style={{color}}>{color}</span></p>
+            <ComponentC/>
         </>
     )
 }
